9000040d0ebaa4b2b466e4a8297f5bfdb5784110
Dwarkesh <143956295+Dwarkesh-Khairnar@users.noreply.github.com>
2026-01-27 00:09:47 +0530

    Add temperature conversion on button click

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const converter_btn = document.querySelector('#converter_btn')
 // const fToCel = (fTemp - 32) * 5 / 9;
 // const cToFahr = cTemp * 9 / 5 + 32;
 
+
 converter_btn.addEventListener('click',()=>{
     converter.value = temprechar.value * 9 / 5 + 32
 })
