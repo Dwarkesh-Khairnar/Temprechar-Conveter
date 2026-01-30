cbb9b85cbf31d9dcfcfaf882fa2fdab35e1125cc
Dwarkesh <143956295+Dwarkesh-Khairnar@users.noreply.github.com>
2026-01-30 08:55:11 +0530

    Uncomment fToCel function for temperature conversion

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const temprechar = document.querySelector('#temprechar')
 const converter = document.querySelector('#converter')
 const converter_btn = document.querySelector('#converter_btn')
-// const fToCel = (fTemp - 32) * 5 / 9;
+ const fToCel = (fTemp - 32) * 5 / 9;
 // const cToFahr = cTemp * 9 / 5 + 32;
 
 
